aa3aa0a92bbfc4484a80b0a2cba46fd8410646d8
NAHID1367 <168970738+NAHID1367@users.noreply.github.com>
2025-12-19 17:41:22 +0330

    Contact Meta
    created ContactAdmin
    Meta

--- blog/models.py
+++ blog/models.py
@@ -14,5 +14,10 @@ class Post(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
     
+    class Meta:
+        ordering = ['-created_date']
+        #verbose_name = 'پست'
+        #verbose_name_plural = 'پستها'
+
     def __str__(self):
-        return "  {} - {} ".format(self.title,self.id)
+        return " {} - {} ".format(self.title,self.id)
