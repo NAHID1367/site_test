d0f814d3661ae3b1e8734fca8421f6dccc309a6c
NAHID1367 <168970738+NAHID1367@users.noreply.github.com>
2025-12-24 18:34:29 +0330

    created test
    created test.html
    edit blog.html
    post id

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
 from  django.urls import path
-from .views import blog_view, single_view 
+from .views import blog_view, single_view, test
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', blog_view,name='index'),
     path('single/',single_view,name='single'),
+    path('post/<int:pid>',test,name='test'),
+    #path('<str:name>/<str:family_name>/<int:age>',test,name='test'),
 ]
