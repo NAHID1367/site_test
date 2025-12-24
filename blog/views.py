f33412f2f369beb33b0515ca54288c0476bcab3d
NAHID1367 <168970738+NAHID1367@users.noreply.github.com>
2025-12-24 20:21:02 +0330

    status
    published status=1

--- blog/views.py
+++ blog/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 
 def blog_view(request):
-    posts = Post.objects.all()
+    posts = Post.objects.filter(status=1)
     context = {'posts':posts}
     return render(request,'blog/blog-home.html',context)
 
 def single_view(request,pid):
+    posts = Post.objects.filter(status=1)
     post = get_object_or_404(Post, pk=pid)
     context = {'post':post}
     return render(request,'blog/blog-single.html', context)
