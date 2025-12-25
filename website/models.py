2e1caea1a0f9271d1d54d63274b9625f96865288
NAHID1367 <168970738+NAHID1367@users.noreply.github.com>
2025-12-25 16:20:27 +0330

    author
    {{post.author.get_full_name}}

--- website/models.py
+++ website/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Contact(models.Model):
     name = models.CharField(max_length=255)
@@ -8,6 +9,7 @@ class Contact(models.Model):
     messge = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
+    
 
     class Meta:
         ordering = ['created_date']
