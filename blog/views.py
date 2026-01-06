d538fd131e68ee4483189e8696190c05b9b0fb94
NAHID1367 <168970738+NAHID1367@users.noreply.github.com>
2026-01-06 20:10:20 +0330

    pagenumber
    pagenotaninteger

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 
-def blog_view(request,cat_name=None,author_username=None):
+def blog_view(request,**kwargs):
     posts = Post.objects.filter(status=1)
-    if cat_name:
-        posts = posts.filter(category__name=cat_name)
-    if author_username:
-        posts = posts.filter(author__username=author_username)
+    if kwargs.get('cat_name') != None:
+        posts = posts.filter(category__name=kwargs['cat_name'])
+    if kwargs.get('author_username') != None:
+        posts = posts.filter(author__username=kwargs['author_username'])
+    posts = Paginator(posts,2)
+    try:
+        page_number = request.GET.get('page')
+        posts = posts.get_page(page_number)
+    except PageNotAnInteger:
+        posts = posts.get_page(1)
+    except EmptyPage:
+        posts = posts.get_page(1)
     context = {'posts':posts}
     return render(request,'blog/blog-home.html',context)
 
