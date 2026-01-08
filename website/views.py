f33429f6fa37e64052e0645dc5474ce588038e94
NAHID1367 <168970738+NAHID1367@users.noreply.github.com>
2026-01-08 19:44:55 +0330

    messages
    messages

--- website/views.py
+++ website/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse,JsonResponse
 from website.models import Contact
 from website.forms import NameForm , ContactForm, NewsleterForm
+from django.contrib import messages
 
 def index_view(request):
     return render(request,'website/index.html')
@@ -14,6 +15,10 @@ def contact_view(request):
         form = ContactForm(request.POST) 
         if form.is_valid():
             form.save()
+            messages.add_message(request,messages.success, 'your ticket submited successfully')
+        else:
+            messages.add_message(request,messages.ERROR, 'your ticket didnt submited')
+
             return redirect('website:contact')  # خیلی مهم
     else:
         form = ContactForm()
